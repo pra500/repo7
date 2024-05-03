b365a5dac6f1fb06f05485ddc6607ecbb5931d01
pragti gupta <pragtig6@gmail.com>
2024-05-03 16:16:58 +0530

    b

---
+++ src/pac1/Stringj.java
@@ -0,0 +1,14 @@
+package pac1;
+
+public class Stringj {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		String str="pragti";
+		System.out.println(str);
+
+	}
+
+}
